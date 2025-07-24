8cb8215c635bd51c817b47a8fcf551a24f2167dc
OG-Vivek <vivek.singh@ongraph.com>
2025-07-24 21:58:10 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        int num = 45;
+        String str = "Hello JAVA :)";
+        System.out.println(num);
+        System.out.println(str);
+    }
+}
